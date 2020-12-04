51ef627abb36751e67a79d3d736cb5b62ac32881
tierre0703 <khj20130703@hotmail.com>
2020-12-04 10:36:52 +0800

    Sqlite to RoomMngtService

--- RoomServiceMngtServiceSolution/ManagementApp/CallMngtUC.cs
+++ RoomServiceMngtServiceSolution/ManagementApp/CallMngtUC.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
+using System.Data.SQLite;
+//using System.Data.SqlClient;
 
 using System.Configuration;
 namespace ManagementApp
